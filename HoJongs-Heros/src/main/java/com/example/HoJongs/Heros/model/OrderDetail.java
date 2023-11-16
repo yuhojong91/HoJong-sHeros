d4f8ff1acee9a135de3024545470924b7f35facd
MacMaster <andrew.macmaster@fiserv.com>
2023-11-16 15:15:02 -0500

    Refactoring to camelCase for HTTP variables

--- HoJongs-Heros/src/main/java/com/example/HoJongs/Heros/model/OrderDetail.java
+++ HoJongs-Heros/src/main/java/com/example/HoJongs/Heros/model/OrderDetail.java
@@ -1,11 +1,6 @@
  package com.example.HoJongs.Heros.model;
 
- import jakarta.persistence.Entity;
- import jakarta.persistence.GeneratedValue;
- import jakarta.persistence.GenerationType;
- import jakarta.persistence.Id;
- import jakarta.persistence.JoinColumn;
- import jakarta.persistence.ManyToOne;
+ import jakarta.persistence.*;
 
  import java.math.BigInteger;
  @Entity
@@ -14,45 +9,46 @@
      @Id
      @GeneratedValue(strategy = GenerationType.IDENTITY)
      private Long Id;
-     private Long order_id;
-     private Long product_id;
+     private Long orderId;
+     @Column(insertable=false, updatable=false)
+     private Long productId;
      private BigInteger quantity;
      private double discount;
      private double price;
 
      @ManyToOne
-     @JoinColumn(name = "order_id", referencedColumnName = "Id")
+     @JoinColumn(name = "orderId", referencedColumnName = "Id")
      private CustomerOrder order;
 
      @ManyToOne
-     @JoinColumn(name = "product_id", referencedColumnName = "Id")
+     @JoinColumn(name = "productId", referencedColumnName = "Id")
      private Product product;
      public OrderDetail(){
      }
 
-     public OrderDetail(BigInteger quantity, double discount, Long order_id, Long product_id) { //Product product removed from constructor for testing
+     public OrderDetail(BigInteger quantity, double discount, Long orderId, Long productId) { //Product product removed from constructor for testing
          this.quantity = quantity;
          this.discount = discount;
          this.price = 0;
-         this.order_id = order_id;
-         this.product_id = product_id;
+         this.orderId = orderId;
+         this.productId = productId;
      }
 
-     public OrderDetail(BigInteger quantity, Long order_id, Long product_id) { //Product product removed from constructor for testing
+     public OrderDetail(BigInteger quantity, Long orderId, Long productId) { //Product product removed from constructor for testing
          this.quantity = quantity;
          this.discount = 0;
          this.price = 0;
-         this.order_id = order_id;
-         this.product_id = product_id;
+         this.orderId = orderId;
+         this.productId = productId;
      }
 
      public Long getId() { return Id; }
 
-     public Long getOrderId() { return order_id; }
-     public void setOrderId(Long order_id) {this.order_id = order_id; }
+     public Long getOrderId() { return orderId; }
+     public void setOrderId(Long order_id) {this.orderId = order_id; }
 
-     public Long getProductId() { return product_id; }
-     public void setProductId(Long product_id) {this.product_id = product_id; }
+     public Long getProductId() { return productId; }
+     public void setProductId(Long product_id) {this.productId = product_id; }
 
      public BigInteger getQuantity() { return quantity; }
      public void setQuantity(BigInteger quantity) { this.quantity = quantity; }
