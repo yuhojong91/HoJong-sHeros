95e92fd1da2f6328a58fd439e4748154df92178f
MacMaster <andrew.macmaster@fiserv.com>
2023-11-16 10:35:21 -0500

    Added OrderDetailRepository interface

---
+++ HoJongs-Heros/src/main/java/com/example/HoJongs/Heros/repository/OrderDetailRepository.java
@@ -0,0 +1,7 @@
+package com.example.HoJongs.Heros.repository;
+
+import com.example.HoJongs.Heros.model.OrderDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
+}
