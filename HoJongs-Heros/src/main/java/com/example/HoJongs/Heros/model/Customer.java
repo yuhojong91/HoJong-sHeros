72616cae9b1b206b821b5e447bc7d643a1acd540
MacMaster <andrew.macmaster@fiserv.com>
2023-11-17 08:22:26 -0500

    Add setters to Customer class

--- HoJongs-Heros/src/main/java/com/example/HoJongs/Heros/model/Customer.java
+++ HoJongs-Heros/src/main/java/com/example/HoJongs/Heros/model/Customer.java
@@ -40,24 +40,30 @@ public class Customer {
     public String getPhoneNumber() {
         return phoneNumber;
     }
+    public void setPhoneNumber(String phoneNumber) {this.phoneNumber = phoneNumber; }
 
     public String getName() {
         return name;
     }
+    public void setName(String name) {this.name = name; }
 
     public String getAddress() {
         return address;
     }
+    public void setAddress(String address) {this.address = address; }
 
     public String getCity() {
         return city;
     }
+    public void setCity(String city) {this.city = city; }
 
     public String getState() {
         return state;
     }
+    public void setState(String state) {this.state = state; }
 
     public String getZipCode() {
         return zipCode;
     }
+    public void setZipCode(String zipCode) {this.zipCode = zipCode; }
 }
